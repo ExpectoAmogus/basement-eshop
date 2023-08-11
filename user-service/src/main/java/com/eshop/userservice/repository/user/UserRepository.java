49a8177f00ef4d1a74028d889a7c95ef465c5c5f
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-11 20:38:45 +0200

    update UserRepository.java

--- user-service/src/main/java/com/eshop/userservice/repository/user/UserRepository.java
+++ user-service/src/main/java/com/eshop/userservice/repository/user/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends BaseRepository<User> {
+public interface UserRepository extends BaseUserRepository<User> {
 
     List<User> findAll();
 
