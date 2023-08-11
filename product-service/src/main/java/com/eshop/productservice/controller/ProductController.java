985fb3ccf60f83d69315621d40cd4e9497b77da7
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-11 17:21:14 +0200

    create ProductController in controllers

---
+++ product-service/src/main/java/com/eshop/productservice/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.eshop.productservice.controller;
+
+import com.eshop.productservice.models.dto.ProductResponse;
+import com.eshop.productservice.service.impl.ProductServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/products")
+public class ProductController {
+    private final ProductServiceImpl productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductResponse>> getProducts(){
+        return ResponseEntity.ok()
+                .body(productService.getProducts());
+    }
+}
