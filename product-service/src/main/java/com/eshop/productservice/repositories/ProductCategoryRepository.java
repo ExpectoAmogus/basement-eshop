ee45bc48943165e171bb82ba1bd12b25a21faf60
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-13 00:53:41 +0200

    create repo ProductCategoryRepository.java

--- product-service/src/main/java/com/eshop/productservice/repositories/ProductCategoryRepository.java
+++ product-service/src/main/java/com/eshop/productservice/repositories/ProductCategoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-    Optional<ProductCategory> findByNameAndParent(String name, ProductCategory parent);
+    Optional<ProductCategory> findByNameAndParentId(String name, Long parentId);
 }
