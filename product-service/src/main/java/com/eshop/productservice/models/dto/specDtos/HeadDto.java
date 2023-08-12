eeee1f322c1e4e14bb2ce8d20244cb5f8320bc85
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-13 00:51:07 +0200

    create specDtos

---
+++ product-service/src/main/java/com/eshop/productservice/models/dto/specDtos/HeadDto.java
@@ -0,0 +1,18 @@
+package com.eshop.productservice.models.dto.specDtos;
+
+import com.eshop.productservice.models.dto.ProductSpecDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HeadDto extends ProductSpecDto {
+
+    private Long headGirth;
+}
