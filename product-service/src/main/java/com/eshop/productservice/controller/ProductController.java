287e5a6c34368bae4f68e812f51d82b3eb93bb78
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-15 22:33:37 +0200

    update ProductController.java

--- product-service/src/main/java/com/eshop/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/eshop/productservice/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/products")
+@RequestMapping("api/product")
 public class ProductController {
     private final ProductService productService;
 
