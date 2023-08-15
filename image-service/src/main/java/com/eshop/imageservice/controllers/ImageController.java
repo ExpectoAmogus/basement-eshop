f8311b7da70d91a98ef4da3dfe5c78c10ea43274
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-15 22:33:24 +0200

    update ImageController.java

--- image-service/src/main/java/com/eshop/imageservice/controllers/ImageController.java
+++ image-service/src/main/java/com/eshop/imageservice/controllers/ImageController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/images")
+@RequestMapping("api/image")
 public class ImageController {
 
     private final ImageService imageService;
