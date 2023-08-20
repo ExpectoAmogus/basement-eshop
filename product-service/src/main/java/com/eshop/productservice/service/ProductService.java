e5dc75f59bdcc8afe297c99ac4f1f70712bd35d1
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-20 18:40:01 +0200

    update ProductService.java

--- product-service/src/main/java/com/eshop/productservice/service/ProductService.java
+++ product-service/src/main/java/com/eshop/productservice/service/ProductService.java
@@ -1,11 +1,12 @@
 package com.eshop.productservice.service;
 
+import com.eshop.productservice.models.dto.ProductCreateResponse;
 import com.eshop.productservice.models.dto.ProductRequest;
 import com.eshop.productservice.models.dto.ProductResponse;
 
 import java.util.List;
 
 public interface ProductService {
-    ProductResponse createProduct(ProductRequest productRequest);
+    ProductCreateResponse createProduct(ProductRequest productRequest);
     List<ProductResponse> getProducts();
 }
