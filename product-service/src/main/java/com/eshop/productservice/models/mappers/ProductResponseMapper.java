cab77559a18163e328639339ba75dc3207bd2d8c
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-26 18:33:06 +0200

    update mapper ProductResponseMapper

--- product-service/src/main/java/com/eshop/productservice/models/mappers/ProductResponseMapper.java
+++ product-service/src/main/java/com/eshop/productservice/models/mappers/ProductResponseMapper.java
@@ -2,12 +2,17 @@ package com.eshop.productservice.models.mappers;
 
 import com.eshop.productservice.models.dto.ProductResponse;
 import com.eshop.productservice.models.entity.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
+@RequiredArgsConstructor
 public class ProductResponseMapper implements Function<Product, ProductResponse> {
+    private final ProductSpecDtoMapper productSpecDtoMapper;
+    private final ProductCategoryDtoMapper productCategoryDtoMapper;
+
     @Override
     public ProductResponse apply(Product product) {
         return new ProductResponse(
@@ -15,8 +20,8 @@ public class ProductResponseMapper implements Function<Product, ProductResponse>
                 product.getName(),
                 product.getCode(),
                 product.getDescription(),
-                product.getProductCategoryDto(),
-                product.getProductSpecDto(),
+                productCategoryDtoMapper.apply(product.getCategory()),
+                productSpecDtoMapper.apply(product.getSpec()),
                 product.getPrice()
                 );
     }
