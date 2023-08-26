a9223cc1cc8451f3adcab869a4adba9ca281f1c9
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-26 18:25:49 +0200

    update controller CartController

--- cart-service/src/main/java/com/eshop/cartservice/controller/CartController.java
+++ cart-service/src/main/java/com/eshop/cartservice/controller/CartController.java
@@ -1,5 +1,7 @@
 package com.eshop.cartservice.controller;
 
+import com.eshop.cartservice.facade.CartFacade;
+import com.eshop.cartservice.models.dto.CartDto;
 import com.eshop.cartservice.models.entity.Cart;
 import com.eshop.cartservice.service.CartService;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +14,14 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequestMapping("api/cart")
 public class CartController {
-    private final CartService cartService;
+    private final CartFacade cartFacade;
 
     @PostMapping("/{userId}/addItem")
     public ResponseEntity<String> addItemToCart(
             @PathVariable String userId,
             @RequestParam String productId,
             @RequestParam int quantity) {
-        cartService.addItemToCart(userId, productId, quantity);
+        cartFacade.addItemToCart(userId, productId, quantity);
         return ResponseEntity.ok("Item added to cart successfully");
     }
 
@@ -27,7 +29,7 @@ public class CartController {
     public ResponseEntity<String> removeItemFromCart(
             @PathVariable String userId,
             @PathVariable String itemId) {
-        cartService.removeItemFromCart(userId, itemId);
+        cartFacade.removeItemFromCart(userId, itemId);
         return ResponseEntity.ok("Item removed from cart successfully");
     }
 
@@ -36,13 +38,13 @@ public class CartController {
             @PathVariable String userId,
             @PathVariable String itemId,
             @RequestParam int newQuantity) {
-        cartService.updateCartItemQuantity(userId, itemId, newQuantity);
+        cartFacade.updateCartItemQuantity(userId, itemId, newQuantity);
         return ResponseEntity.ok("Item quantity updated successfully");
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Cart> getCartByUserId(@PathVariable String userId) {
-        Cart cart = cartService.getCartByUserId(userId);
+    public ResponseEntity<CartDto> getCartByUserId(@PathVariable String userId) {
+        CartDto cart = cartFacade.getCartByUserId(userId);
         return ResponseEntity.ok(cart);
     }
 }
