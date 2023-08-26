4a8d044b8421a0038b10970530c425ed16e8d941
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-26 18:02:51 +0200

    update controller ArticleController

--- article-service/src/main/java/com/eshop/articleservice/controller/ArticleController.java
+++ article-service/src/main/java/com/eshop/articleservice/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.eshop.articleservice.controller;
 
+import com.eshop.articleservice.facade.ArticleFacade;
 import com.eshop.articleservice.models.dto.ArticleRequest;
 import com.eshop.articleservice.models.dto.ArticleResponse;
 import com.eshop.articleservice.models.dto.ArticleToUpdateRequest;
@@ -15,33 +16,33 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("api/article")
 public class ArticleController {
-    private final ArticleService articleService;
+    private final ArticleFacade articleFacade;
 
     @PostMapping("/add")
     public ResponseEntity<String> createArticle(@RequestBody ArticleRequest articleRequest){
-        articleService.create(articleRequest);
+        articleFacade.create(articleRequest);
         return new ResponseEntity<>("Article created!", HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
     public ResponseEntity<String> updateArticle(@RequestBody ArticleToUpdateRequest articleRequest){
-        articleService.update(articleRequest);
+        articleFacade.update(articleRequest);
         return new ResponseEntity<>("Article updated!", HttpStatus.OK);
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteArticle(@RequestParam Long id){
-        articleService.delete(id);
+        articleFacade.delete(id);
         return new ResponseEntity<>("Article deleted!", HttpStatus.OK);
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<ArticleResponse>> getArticles(){
-        return new ResponseEntity<>(articleService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(articleFacade.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ArticleResponse> getArticleById(@PathVariable Long id){
-        return new ResponseEntity<>(articleService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(articleFacade.findById(id), HttpStatus.OK);
     }
 }
