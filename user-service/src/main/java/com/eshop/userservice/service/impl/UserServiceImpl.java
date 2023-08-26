3880c3ad80be0fb7c778572d6a2567ee245913a2
VladLevchenko <clernovmail@gmail.com>
2023-08-26 17:48:43 +0300

    added annotations in UserServiceImpl

--- user-service/src/main/java/com/eshop/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/eshop/userservice/service/impl/UserServiceImpl.java
@@ -4,9 +4,13 @@ import com.eshop.userservice.models.entity.User;
 import com.eshop.userservice.repository.user.UserRepository;
 import com.eshop.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +23,8 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
     public void create(User user) {
         if (userRepository.existsByEmail(user.getEmail())){
             throw new RuntimeException("This user already exist");
@@ -67,11 +73,15 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
+
     public List<User> findAllByListId(List<Long> ids) {
         return userRepository.findAllById(ids);
     }
 
     @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Transactional(readOnly = true)
     public List<User> findAll() {
         return userRepository.findAll();
     }
