fd67db203af54faa0062e576fb12de15584153c6
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-08-31 19:45:20 +0200

    rewrite gateway config from properties to java class format, add jwt filter

---
+++ api-gateway/src/main/java/com/eshop/apigateway/config/GatewayConfig.java
@@ -0,0 +1,56 @@
+package com.eshop.apigateway.config;
+
+import com.eshop.apigateway.jwt.JwtFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, JwtFilter jwtFilter){
+        return builder.routes()
+                .route("product-service", r -> r.path("/api/product/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://product-service"))
+
+                .route("article-service", r -> r.path("/api/article/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://article-service"))
+
+                .route("cart-service", r -> r.path("/api/cart/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://cart-service"))
+
+                .route("image-service", r -> r.path("/api/image/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://image-service"))
+
+                .route("inventory-service", r -> r.path("/api/inventory/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://inventory-service"))
+
+                .route("order-service", r -> r.path("/api/order/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://order-service"))
+
+                .route("ticket-service", r -> r.path("/api/ticket/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://ticket-service"))
+
+                .route("user-service", r -> r.path("/api/user/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("lb://user-service"))
+
+                .route("discovery-server", r -> r.path("/eureka/web")
+                        .filters(f -> f.setPath("/"))
+                        .uri("http://localhost:8761"))
+
+                .route("discovery-server-static", r -> r.path("/eureka/**")
+                        .uri("http://localhost:8761"))
+
+                .build();
+    }
+}
---
+++ api-gateway/src/main/java/com/eshop/apigateway/jwt/JwtGatewayTokenProvider.java
@@ -0,0 +1,69 @@
+package com.eshop.apigateway.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+
+@Service
+public class JwtGatewayTokenProvider {
+    private static final String SECRET_KEY = "2D4B6150645367566B59703373367639792442264528482B4D6251655468576D";
+
+    public boolean isTokenValid(String token) {
+        final String username = extractUsername(token);
+        return !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractTokenFromRequest(ServerHttpRequest request) {
+        List<String> authorizationHeaders = request.getHeaders().get(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeaders != null && !authorizationHeaders.isEmpty()) {
+            String authorizationHeader = authorizationHeaders.get(0);
+
+            if (authorizationHeader.startsWith("Bearer ")) {
+                return authorizationHeader.substring(7);
+            }
+        }
+
+        return null;
+    }
+}
+
