0ebbc573fbf6e9ed49f6488e6c58a3342cc27159
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-05 20:32:04 +0200

    create security config and filters for jwt validation

---
+++ inventory-service/src/main/java/com/eshop/inventoryservice/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.eshop.inventoryservice.config;
+
+import com.eshop.inventoryservice.jwt.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/api/inventory/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> response
+                                .sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"))
+                );
+        return http.build();
+    }
+}
---
+++ inventory-service/src/main/java/com/eshop/inventoryservice/jwt/JwtInventoryTokenProvider.java
@@ -0,0 +1,79 @@
+package com.eshop.inventoryservice.jwt;
+
+import com.eshop.common.security.jwt.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtInventoryTokenProvider implements JwtTokenProvider{
+    @Value("${secret.key.jwt}")
+    private String SECRET_KEY;
+
+    @Override
+    public boolean isTokenValid(String token) {
+        return !isTokenExpired(token);
+    }
+
+    @Override
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> extractRoles(String token) {
+        final Claims claims = extarctAllClaims(token);
+
+        if (claims.get("role") instanceof Collection<?> roles) {
+            return roles.stream()
+                    .filter(role -> role instanceof Map) // Ensure it's a Map
+                    .map(role -> (Map<?, ?>) role) // Cast to Map
+                    .filter(roleMap -> roleMap.containsKey("authority")) // Ensure it has "authority" key
+                    .map(roleMap -> roleMap.get("authority").toString()) // Extract "authority" value as String
+                    .map(SimpleGrantedAuthority::new)
+                    .toList();
+        }
+        return Collections.emptyList();
+
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extarctAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extarctAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
