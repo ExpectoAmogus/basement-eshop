00a3eea3c0d1dee74fc3062125e831d46c6a0ec6
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-07 13:51:53 +0200

    Create InventoryRequest.java

---
+++ product-service/src/main/java/com/eshop/productservice/models/dto/InventoryRequest.java
@@ -0,0 +1,15 @@
+package com.eshop.productservice.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+public class InventoryRequest {
+    private String code;
+    private Integer quantity;
+}
