6dd6ce5970f05a7792dfc09ed9b42ae82e889d06
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-07 13:51:58 +0200

    Create KafkaConsumerConfig.java

---
+++ inventory-service/src/main/java/com/eshop/inventoryservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,57 @@
+package com.eshop.inventoryservice.config;
+
+import com.eshop.inventoryservice.models.dto.InventoryRequest;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String kafkaGroupId;
+
+    public Map<String, Object> consumerConfig() {
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroupId);
+
+        //Deserializer props
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.eshop.inventoryservice.models.dto.InventoryRequest");
+        props.put(JsonDeserializer.TYPE_MAPPINGS, "com.eshop.productservice.models.dto.InventoryRequest:com.eshop.inventoryservice.models.dto.InventoryRequest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.eshop.productservice");
+
+        return props;
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, InventoryRequest> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, InventoryRequest> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+}
