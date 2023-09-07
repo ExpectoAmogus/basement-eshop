69ad3cb6f35ef50db3b2ad31d126b0134117be23
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-07 22:18:39 +0200

    Update InventoryFacadeImpl.java

--- inventory-service/src/main/java/com/eshop/inventoryservice/facade/impl/InventoryFacadeImpl.java
+++ inventory-service/src/main/java/com/eshop/inventoryservice/facade/impl/InventoryFacadeImpl.java
@@ -6,9 +6,6 @@ import com.eshop.inventoryservice.models.dto.InventoryResponse;
 import com.eshop.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +13,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@EnableKafka
 public class InventoryFacadeImpl implements InventoryFacade {
     private final InventoryService inventoryService;
 
@@ -26,8 +22,7 @@ public class InventoryFacadeImpl implements InventoryFacade {
     }
 
     @Override
-    @KafkaListener(topics = "inventory-topic", groupId = "inventoryId", containerFactory = "kafkaListenerContainerFactory")
-    public void create(@Payload InventoryRequest request) {
+    public void create(InventoryRequest request) {
         inventoryService.create(request.getCode(), request.getQuantity());
     }
 
