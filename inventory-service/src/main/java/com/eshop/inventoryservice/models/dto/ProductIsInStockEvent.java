e5b8a888b4ed272a375c578884363df6f2d37079
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-07 22:18:49 +0200

    Create ProductIsInStockEvent.java

---
+++ inventory-service/src/main/java/com/eshop/inventoryservice/models/dto/ProductIsInStockEvent.java
@@ -0,0 +1,16 @@
+package com.eshop.inventoryservice.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductIsInStockEvent {
+    private List<String> code;
+}
