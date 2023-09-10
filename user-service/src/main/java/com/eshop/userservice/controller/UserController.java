8b38ecd445381a7ebfaa98da2284805df2622b07
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-10 21:38:18 +0200

    Update UserController.java

--- user-service/src/main/java/com/eshop/userservice/controller/UserController.java
+++ user-service/src/main/java/com/eshop/userservice/controller/UserController.java
@@ -4,6 +4,7 @@ import com.eshop.userservice.facade.UserFacade;
 import com.eshop.userservice.service.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserController {
     private final UserFacade userFacade;
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('full','write')")
     public ResponseEntity<List<CustomUserDetails>> getAllUsers() {
         return ResponseEntity.ok().body(userFacade.findAll());
     }
