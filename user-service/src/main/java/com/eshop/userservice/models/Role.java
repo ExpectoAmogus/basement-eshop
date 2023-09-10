090a2c9718f89989789cdac4953496e4789e65f4
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-10 21:38:32 +0200

    Update Role.java

--- user-service/src/main/java/com/eshop/userservice/models/Role.java
+++ user-service/src/main/java/com/eshop/userservice/models/Role.java
@@ -1,5 +1,26 @@
 package com.eshop.userservice.models;
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
 public enum Role {
-    ROLE_ADMIN, ROLE_USER, ROLE_SUPERADMIN
+    ROLE_ADMIN(Set.of(Permission.READ, Permission.WRITE)),
+    ROLE_USER(Set.of(Permission.READ)),
+    ROLE_SUPERADMIN(Set.of(Permission.FULL_ACCESS));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
