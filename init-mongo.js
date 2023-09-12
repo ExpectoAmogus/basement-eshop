c77a5d2088935f9a4933d5bc2545b15f37c201e7
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-12 15:02:07 +0200

    update docker props for mongodb

--- init-mongo.js
+++ init-mongo.js
@@ -1,12 +1,25 @@
+print('Start creating database ##########################')
+db = db.getSiblingDB('cart_service');
 db.createUser(
     {
-        user: "mongo",
-        pwd: "admin",
-        roles: [
-            {
-                role: "readWrite",
-                db: "cart_service"
-            }
-        ]
+        user: 'mongo',
+        pwd:  'admin',
+        roles: [{role: 'readWrite', db: 'cart_service'}],
     }
-)
+);
+db = new Mongo().getDB("cart_service");
+db.createCollection('carts');
+db.createCollection('cart_items');
+
+db = db.getSiblingDB('product_service');
+db.createUser(
+    {
+        user: 'mongo',
+        pwd:  'admin',
+        roles: [{role: 'readWrite', db: 'product_service'}],
+    }
+);
+db = new Mongo().getDB("product_service");
+db.createCollection('products');
+db.createCollection('categories');
+print('End creating database ##########################')
