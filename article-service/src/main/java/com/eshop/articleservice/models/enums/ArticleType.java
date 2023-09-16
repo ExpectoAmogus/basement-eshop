49c496d479b7874657b5e0a268348df499aaf379
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-16 19:56:52 +0200

    Update ArticleType.java

--- article-service/src/main/java/com/eshop/articleservice/models/enums/ArticleType.java
+++ article-service/src/main/java/com/eshop/articleservice/models/enums/ArticleType.java
@@ -1,5 +1,5 @@
 package com.eshop.articleservice.models.enums;
 
 public enum ArticleType {
-    NEWS_POST, SPECIAL_OFFER_POST, BLOG_POST, DISCOUNT_POST, UNDEFINED
+    NEWS_POST, SPECIAL_OFFER_POST, BLOG_POST
 }
