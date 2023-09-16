7dbb4c7a4878ba2e6ccf791777a2ccf55625d6fc
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-16 19:57:28 +0200

    update dtos

--- article-service/src/main/java/com/eshop/articleservice/models/dto/ArticleRequest.java
+++ article-service/src/main/java/com/eshop/articleservice/models/dto/ArticleRequest.java
@@ -1,11 +1,9 @@
 package com.eshop.articleservice.models.dto;
 
-import com.eshop.articleservice.models.enums.ArticleType;
-
 public record ArticleRequest(
         String title,
         String subTitle,
         String text,
-        ArticleType type
+        String type
 ) {
 }
--- article-service/src/main/java/com/eshop/articleservice/models/dto/ArticleToUpdateRequest.java
+++ article-service/src/main/java/com/eshop/articleservice/models/dto/ArticleToUpdateRequest.java
@@ -1,12 +1,10 @@
 package com.eshop.articleservice.models.dto;
 
-import com.eshop.articleservice.models.enums.ArticleType;
-
 public record ArticleToUpdateRequest(
         Long id,
         String title,
         String subTitle,
         String text,
-        ArticleType type
+        String type
 ) {
 }
