19ad72526a47fce0d1e9bc8dd977a1b5e1d3ebec
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-16 19:56:59 +0200

    Update ArticleResponseMapper.java

--- article-service/src/main/java/com/eshop/articleservice/models/mappers/ArticleResponseMapper.java
+++ article-service/src/main/java/com/eshop/articleservice/models/mappers/ArticleResponseMapper.java
@@ -1,15 +1,15 @@
 package com.eshop.articleservice.models.mappers;
 
 import com.eshop.articleservice.models.dto.ArticleResponse;
-import com.eshop.articleservice.models.entity.Article;
+import com.eshop.articleservice.models.entity.BaseArticle;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
-public class ArticleResponseMapper implements Function<Article, ArticleResponse> {
+public class ArticleResponseMapper implements Function<BaseArticle, ArticleResponse> {
     @Override
-    public ArticleResponse apply(Article article) {
+    public ArticleResponse apply(BaseArticle article) {
         return new ArticleResponse(
                 article.getId(),
                 article.getDateOfCreated(),
