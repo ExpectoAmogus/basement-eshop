05b503936a95a46050bcf97487f328e758fd73fa
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-16 19:57:46 +0200

    Update ArticleFacadeImpl.java

--- article-service/src/main/java/com/eshop/articleservice/facade/impl/ArticleFacadeImpl.java
+++ article-service/src/main/java/com/eshop/articleservice/facade/impl/ArticleFacadeImpl.java
@@ -1,11 +1,12 @@
 package com.eshop.articleservice.facade.impl;
 
 import com.eshop.articleservice.facade.ArticleFacade;
+import com.eshop.articleservice.factory.ArticleFactory;
 import com.eshop.articleservice.models.dto.ArticleCreateResponse;
 import com.eshop.articleservice.models.dto.ArticleRequest;
 import com.eshop.articleservice.models.dto.ArticleResponse;
 import com.eshop.articleservice.models.dto.ArticleToUpdateRequest;
-import com.eshop.articleservice.models.entity.Article;
+import com.eshop.articleservice.models.entity.BaseArticle;
 import com.eshop.articleservice.models.mappers.ArticleResponseMapper;
 import com.eshop.articleservice.service.ArticleService;
 import jakarta.persistence.EntityNotFoundException;
@@ -21,48 +22,38 @@ import java.util.List;
 public class ArticleFacadeImpl implements ArticleFacade {
     private final ArticleService articleService;
     private final ArticleResponseMapper articleResponseMapper;
+    private final List<ArticleFactory> articleFactories;
 
     @Override
     public ArticleCreateResponse create(ArticleRequest articleRequest) {
-        Article article = Article.builder()
-                .title(articleRequest.title())
-                .subTitle(articleRequest.subTitle())
-                .text(articleRequest.text())
-                .type(articleRequest.type())
-                .build();
-        Article newArticle = articleService.create(article);
-        return new ArticleCreateResponse(newArticle.getId(), newArticle.getType().toString());
+        for (ArticleFactory factory : articleFactories) {
+            if (articleRequest.type().equals(String.valueOf(factory.getArticleType()))) {
+                var newArticle = articleService.create(factory.createArticle(articleRequest));
+                return new ArticleCreateResponse(newArticle.getId(), newArticle.getType().toString());
+            }
+        }
+        throw new IllegalArgumentException("Invalid article type: " + articleRequest.type());
     }
 
     @Override
     public void update(ArticleToUpdateRequest articleToUpdate) {
-        try{
-            Article existingArticle = articleService.findById(articleToUpdate.id());
-
-            Article article = Article.builder()
-                    .id(existingArticle.getId())
-                    .title(articleToUpdate.title())
-                    .subTitle(articleToUpdate.subTitle())
-                    .text(articleToUpdate.text())
-                    .type(articleToUpdate.type())
-                    .build();
-
-            articleService.update(article);
-        }
-        catch (EntityNotFoundException e){
+        try {
+            BaseArticle existingArticle = articleService.findById(articleToUpdate.id());
+            for (ArticleFactory factory : articleFactories) {
+                if (articleToUpdate.type().equals(String.valueOf(factory.getArticleType()))) {
+                    articleService.update(factory.updateArticle(articleToUpdate, existingArticle.getId()));
+                }
+            }
+        } catch (EntityNotFoundException e) {
             log.error("Article does not exist!");
         }
-
     }
 
     @Override
     public void delete(Long id) {
         try {
-            Article articleToDelete = articleService.findById(id);
-
-            articleService.delete(articleToDelete);
-        }
-        catch (EntityNotFoundException e){
+            articleService.delete(articleService.findById(id));
+        } catch (EntityNotFoundException e) {
             log.error("Article does not exist!");
         }
 
