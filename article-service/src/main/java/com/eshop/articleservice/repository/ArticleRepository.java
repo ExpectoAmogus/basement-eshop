688d3f08c5cf63a4690a11ffea5d548b2c4fdd7d
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-16 19:57:16 +0200

    Update ArticleRepository.java

--- article-service/src/main/java/com/eshop/articleservice/repository/ArticleRepository.java
+++ article-service/src/main/java/com/eshop/articleservice/repository/ArticleRepository.java
@@ -1,7 +1,7 @@
 package com.eshop.articleservice.repository;
 
-import com.eshop.articleservice.models.entity.Article;
+import com.eshop.articleservice.models.entity.BaseArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<BaseArticle, Long> {
 }
