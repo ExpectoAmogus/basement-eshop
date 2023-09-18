ff80488bb497fb2b2d0be813acd5119de16ab895
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-09-18 17:47:50 +0200

    Update BaseArticle.java

--- article-service/src/main/java/com/eshop/articleservice/models/entity/BaseArticle.java
+++ article-service/src/main/java/com/eshop/articleservice/models/entity/BaseArticle.java
@@ -4,7 +4,6 @@ import com.eshop.articleservice.models.enums.ArticleType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
